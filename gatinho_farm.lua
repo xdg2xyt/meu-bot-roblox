1a9a91e4434b180ff26b6a63dcc2b0b1bad8e4d7
xdg2xyt <xdg2xyt@gmail.com>
2026-05-30 20:42:27 -0300

    Update gatinho_farm.lua

--- gatinho_farm.lua
+++ gatinho_farm.lua
@@ -341,7 +341,7 @@ local function iniciarColeta()
 
         task.spawn(function()
             local sucesso, erro = pcall(function()
-                loadstring(game:HttpGet(LINK_GITHUB_RAW))()
+                loadstring(game:HttpGet(https://github.com/xdg2xyt/meu-bot-roblox/raw/main/gatinho_farm.lua))()
             end)
             if not sucesso then
                 warn("[GatinhoBot] Erro ao baixar script: " .. tostring(erro))
